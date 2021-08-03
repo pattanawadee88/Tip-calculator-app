6a4dbd052a3f21f7f9fb0077a50b91848adae39f
pattanawadee88 <winyaratp@wit.edu>
2021-08-02 21:44:54 -0400

    updated doCal()

--- script.js
+++ script.js
@@ -3,25 +3,21 @@ const inputNumPerson = document.getElementById('input-person');
 const resetBtn = document.getElementById('reset-btn');
 const tipTotalAmountPerPerson = document.getElementById('tip-amount');
 const totalAmountPerPerson = document.getElementById('total-amount');
-inputNumPerson.value = 0;
-function tipAmount(value){
-    switch(value){
-        case 5/100: 
-            doCal(105/100);
-            break;
-        case 10/100:
-            doCal(110/100);
-            break;  
-        case 15/100:
-            doCal(115/100);
-            break; 
-        case 25/100:
-            doCal(125/100);
-            break; 
-        case 50/100:
-            doCal(150/100);
-            break;  
-    }
+
+// Calculate tip
+const doCal = (tip) => {
+    if(inputNumPerson.value === "" || inputNumPerson.value === null ){
+        inputNumPerson.style.border = '2px solid red';
+        inputNumPerson.style.fontSize = '0.9rem';
+        inputNumPerson.placeholder = 'Please enter number of person';
+    }else {
+        const total = (inputBill.value * (tip));
+        const totalPerPerson = total/inputNumPerson.value;
+        const tipPerPerson = total-(inputBill.value);
+        
+        totalAmountPerPerson.innerHTML = `$`+ Math.round(totalPerPerson);
+        tipTotalAmountPerPerson.innerHTML = `$` + Math.round(tipPerPerson);
+    }    
 }
 
 // Clear input value
@@ -29,18 +25,6 @@ resetBtn.addEventListener('click', ()=>{
     inputBill.value = '';
     inputNumPerson.value = '';
 } )
-
-console.log(inputNumPerson.value)
-const doCal = (tip) => {
-    const total = (inputBill.value * (tip));
-    const totalPerPerson = total/inputNumPerson.value;
-    const tipPerPerson = total-(inputBill.value);
-    
-
-    totalAmountPerPerson.innerHTML = Math.round(totalPerPerson);
-    tipTotalAmountPerPerson.innerHTML = Math.round(tipPerPerson);
-    
-}
 /* 
 1. get values from input 
 2. user selects tip
