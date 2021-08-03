561a3ef85e9a5c554a14a2d7487d6506a4439ff3
pattanawadee88 <winyaratp@wit.edu>
2021-08-02 21:10:40 -0400

    added functions

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 const inputBill = document.getElementById('input-bill');
 const inputNumPerson = document.getElementById('input-person');
 const resetBtn = document.getElementById('reset-btn');
-// const inputBillValue = inputBill.value;
-
-// console.log(inputBillValue)
-function getValue(){
-    console.log(inputBill.value)
-    console.log(inputNumPerson.value)
+const tipTotalAmountPerPerson = document.getElementById('tip-amount');
+const totalAmountPerPerson = document.getElementById('total-amount');
+inputNumPerson.value = 0;
+function tipAmount(value){
+    switch(value){
+        case 5/100: 
+            doCal(105/100);
+            break;
+        case 10/100:
+            doCal(110/100);
+            break;  
+        case 15/100:
+            doCal(115/100);
+            break; 
+        case 25/100:
+            doCal(125/100);
+            break; 
+        case 50/100:
+            doCal(150/100);
+            break;  
+    }
 }
 
 // Clear input value
 resetBtn.addEventListener('click', ()=>{
-    document.querySelector('input').value = '';
+    inputBill.value = '';
+    inputNumPerson.value = '';
 } )
 
+console.log(inputNumPerson.value)
+const doCal = (tip) => {
+    const total = (inputBill.value * (tip));
+    const totalPerPerson = total/inputNumPerson.value;
+    const tipPerPerson = total-(inputBill.value);
+    
 
+    totalAmountPerPerson.innerHTML = Math.round(totalPerPerson);
+    tipTotalAmountPerPerson.innerHTML = Math.round(tipPerPerson);
+    
+}
 /* 
 1. get values from input 
 2. user selects tip
